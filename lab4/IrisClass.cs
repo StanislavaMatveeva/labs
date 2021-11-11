24b81bdddbade822c05acf46e232393524eed679
StanislavaMatveeva <91095038+StanislavaMatveeva@users.noreply.github.com>
2021-11-11 16:00:31 +0300

    new added

---
+++ lab4/IrisClass.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using LinearAlgebra;
+using System.Globalization;
+using System.Management;
+
+namespace IrisGraphics
+{
+    public class IrisClass
+    {
+        MathVector[] values;
+
+
+        /// <summary>
+        /// Contains the enumeration of different values, that are used for working with IrisClass.
+        /// </summary>
+        public enum Numbers
+        {
+            SETOSA = 1,
+            VERSICOLOR = 2,
+            VIRGINICA = 3,
+            SEPAL_LENGTH = 4,
+            SEPAL_WIDTH = 5,
+            PETAL_LENGTH = 6,
+            PETAL_WIDTH = 6,
+            IRIS_SIZE = 50,
+            IRIS_ARRAY_SIZE = 3,
+            METRICS_AMOUNT = 4
+        }
+
+        /// <summary>
+        /// Creates an object of IrisClass.
+        /// </summary>
+        public IrisClass(int size)
+        {
+            values = new MathVector[size];
+            for (int i = 0; i < values.Length; i++)
+                values[i] = new MathVector((int)Numbers.IRIS_SIZE * size);
+        }
+
+        /// <summary>
+        /// Gets an amount of metrics in iris.
+        /// </summary>
+        public int Length { get { return values.Length; } }
+
+        /// <summary>
+        /// Overloads the operator[].
+        /// </summary>
+        /// <param name="i">Index of the current metric.</param>
+        /// <returns>A MathVector, that contains values of the current iris's metric.</returns>
+        public MathVector this[int i] { get { return values[i]; } }
+
+        /// <summary>
+        /// Checks if file consists necessary headers or not.
+        /// </summary>
+        /// <param name="file">Array of string, that contains file's data.</param>
+        /// <returns>True if file contains necessary headers and false if doesn't.</returns>
+        public void CheckData(string[] file)
+        {
+            string check;
+            try { check = file[0]; }
+            catch { throw new Exception("This file is empty"); }
+            if (!check.Contains("sepal_length,sepal_width,petal_length,petal_width,species"))
+                throw new Exception("This file doesn't contains necessary headers");
+        }
+
+        /// <summary>
+        /// (Available only for Windows OS) Checks if file is valid or not.
+        /// </summary>
+        /// <exception cref="Exception">Is thrown, when file's extension is not ".txt"
+        /// or it's volume or bigger than available RAM and virtual memory of the computer.</exception>
+        /// <param name="fileName">Name of file.</param>
+        public void CheckFile(string fileName)
+        {
+            if (!File.Exists(fileName))
+            {
+                var createFile = new string[] { "New", "File" };
+                File.WriteAllLines(fileName, createFile);
+            }
+            else if (new FileInfo(fileName).Extension != ".txt")
+                throw new Exception("Wrong format of file");
+            else
+            {
+                int memory = 0;
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_OperatingSystem");
+                ManagementObjectCollection results = searcher.Get();
+                foreach (ManagementObject result in results)
+                {
+                    var obj = result["FreePhysicalMemory"];
+                    memory += Convert.ToInt32(obj);
+                    obj = result["FreeVirtualMemory"];
+                    memory += Convert.ToInt32(obj);
+                }
+                if (new FileInfo(fileName).Length > memory)
+                    throw new Exception("This file is too big");
+            }
+        }
+
+        public int DetermineIrisMembership(string irisName, int i)
+        {
+            int check;
+            if (irisName == "setosa")
+                check = 0;
+            else if (irisName == "versicolor")
+                check = 1;
+            else if (irisName == "virginica")
+                check = 2;
+            else
+                throw new Exception("Unvalid name of iris");
+            return check;
+        }
+
+        public IrisClass ResizeIris(IrisClass iris, int amount)
+        {
+            var newIris = new IrisClass(iris.Length);
+            for (int i = 0; i < iris.Length; i++)
+            {
+                newIris.values[i] = new MathVector(amount);
+                for (int j = 0; j < amount; j++)
+                    newIris.values[i][j] = iris.values[i][j];
+            }
+            return newIris;
+        }
+
+        /// <summary>
+        /// Creates an iris: fills it with data for the current iris.
+        /// </summary>
+        /// <param name="iris">The current iris.</param>
+        /// <param name="array">Array of string, that contains file's data.</param>
+        /// <param name="irisNumber">A number of the current iris.</param>
+        /// <returns>An array of MathVector, that contains the current iris's values.</returns>
+        public IrisClass CreateIris(IrisClass iris, string[] array, int irisNumber)
+        {
+            if (iris.Length == 0)
+                throw new Exception("This iris is empty");
+            else if (irisNumber < 0)
+                throw new Exception("The number of vector can't be negative");
+            else
+            {
+                int k = 0;
+                int amount = 0;
+                var newIris = new IrisClass(iris.Length);
+                for (int i = 1; i < array.Length; i++)
+                {
+                    var currentString = array[i].Split(',');
+                    if (currentString.Length < (int)Numbers.METRICS_AMOUNT + 1)
+                        throw new Exception("Wrong amount of metrics in string");
+                    else if (irisNumber == DetermineIrisMembership(currentString[(int)Numbers.METRICS_AMOUNT], i))
+                    {
+                        for (int j = 0; j < (int)Numbers.METRICS_AMOUNT; j++)
+                        {
+                            try { newIris[j][k] = double.Parse(currentString[j], new NumberFormatInfo { NumberDecimalSeparator = "." }); }
+                            catch { throw new Exception("Wrong value of data"); }
+                        }
+                        amount++;
+                        k++;
+                    }
+                }
+                return ResizeIris(newIris, amount);
+            }
+        }
+
+        /// <summary>
+        /// Gets an average value of one iris's metric.
+        /// </summary>
+        /// <param name="vector">A MathVector, that contains values of one iris's metric.</param>
+        /// <returns>An anerage score of one iris's metric.</returns>
+        public double GetAvgValue(MathVector vector)
+        {
+            if (vector.Dimensions == 0)
+                throw new Exception("This vector is empty");
+            else
+            {
+                double avgValue = 0;
+                for (int i = 0; i < vector.Dimensions; i++)
+                    avgValue += vector[i];
+                return avgValue /= vector.Dimensions;
+            }
+        }
+
+        /// <summary>
+        /// Creates a MathVector, that contains average values of all iris's metrics.
+        /// </summary>
+        /// <param name="iris">An array of irisis.</param>
+        /// <returns>A MathVector, that contains average values of all iris's metrics.</returns>
+        public MathVector CreateAvgVector(MathVector[] iris)
+        {
+            if (iris.Length == 0)
+                throw new Exception("This array is Empty");
+            else
+            {
+                var avgVector = new MathVector((int)Numbers.METRICS_AMOUNT);
+                for (int i = 0; i < avgVector.Dimensions; i++)
+                    avgVector[i] = GetAvgValue(iris[i]);
+                return avgVector;
+            }
+        }
+
+        /// <summary>
+        /// Creates an array of MathVectors, that contains average vectors of all irisis.
+        /// </summary>
+        /// <param name="iris">An array of irisis.</param>
+        /// <returns>An array of MathVectors, that contains average vectors of all irisis.</returns>
+        public MathVector[] CreateAvgIris(IrisClass[] iris)
+        {
+            if (iris.Length == 0)
+                throw new Exception("This iris is empty");
+            else
+            {
+                var avgIris = new MathVector[(int)Numbers.IRIS_ARRAY_SIZE];
+                for (int i = 0; i < avgIris.Length; i++)
+                    avgIris[i] = CreateAvgVector(iris[i].values);
+                return avgIris;
+            }
+        }
+
+        /// <summary>
+        /// Creates an array of irisis and fills it with file's data.
+        /// </summary>
+        /// <param name="fileName">File's name.</param>
+        /// <returns>An array of irisis, filled with file's data.</returns>
+        public IrisClass[] CreateData(string fileName)
+        {
+            var array = File.ReadAllLines(fileName);
+            var irisArray = new IrisClass[(int)Numbers.IRIS_ARRAY_SIZE];
+            for (int i = 0; i < irisArray.Length; i++)
+            {
+                irisArray[i] = new IrisClass((int)Numbers.METRICS_AMOUNT);
+                irisArray[i] = CreateIris(irisArray[i], array, i);
+            }
+            return irisArray;
+        }
+
+        /// <summary>
+        /// Gets an array of distances between irisis average vectors.
+        /// </summary>
+        /// <param name="iris">An array of irisis.</param>
+        /// <returns>An array of distances between irisis average vectors.</returns>
+        public double[] GetDistancesArray(IrisClass[] iris)
+        {
+            var newIris = CreateAvgIris(iris);
+            var values = new double[newIris.Length];
+            for (int i = 0; i < newIris.Length - 1; i++)
+                values[i] = newIris[i].CalcDistance(newIris[i + 1]);
+            values[values.Length - 1] = newIris[0].CalcDistance(newIris[values.Length - 1]);
+            return values;
+        }
+    }
+}
---
+++ lab4/IrisGraphics.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LinearAlgebra;
+using System.IO;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace IrisGraphics
+{
+    public partial class IrisGraphics : Form
+    {
+        IrisClass ir = new IrisClass((int)IrisClass.Numbers.METRICS_AMOUNT);
+        Executor ex = new Executor();
+        string fileName;
+
+        public IrisGraphics()
+        {
+            InitializeComponent();
+            CleanAll();
+            richTextBoxErrors.Visible = false;
+        }
+
+        private void buttonSepalLength_Click(object sender, EventArgs e)
+        {
+            sepalLengthChart.Series.Clear();
+            sepalLengthChart.Titles.Clear();
+            ChooseBarChart(Executor.Opearations.CREATE_SEPAL_LENGTH_CHART, "Sepal length", sepalLengthChart);
+        }
+
+        private void buttonSepalWidth_Click(object sender, EventArgs e)
+        {
+            sepalWidthChart.Series.Clear();
+            sepalWidthChart.Titles.Clear();
+            ChooseBarChart(Executor.Opearations.CREATE_SEPAL_WIDTH_CHART, "Sepal width", sepalWidthChart);
+        }
+
+        private void buttonPetalLength_Click(object sender, EventArgs e)
+        {
+            petalLengthChart.Series.Clear();
+            petalLengthChart.Titles.Clear();
+            ChooseBarChart(Executor.Opearations.CREATE_PETAL_LENGTH_CHART, "Petal length", petalLengthChart);
+        }
+
+        private void buttonPetalWidth_Click(object sender, EventArgs e)
+        {
+            petalWidthChart.Series.Clear();
+            petalWidthChart.Titles.Clear();
+            ChooseBarChart(Executor.Opearations.CREATE_PETAL_WIDTH_CHART, "Petal width", petalWidthChart);
+        }
+
+        /// <summary>
+        /// Creates a pie chart for distances between average irisis' vectors.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonDistance_Click(object sender, EventArgs e)
+        {
+            CreatePieChart();
+        }
+
+        private void buttonChooseFile_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+            openFileDialog1.ShowDialog();
+            fileName = openFileDialog1.FileName;
+            try { ir.CheckFile(fileName); }
+            catch (Exception ex) { TellAboutError(ex); return; }
+            try { ir.CheckData(File.ReadAllLines(fileName)); }
+            catch (Exception ex) { TellAboutError(ex); return; }
+            richTextBoxFileName.Text = "You choosed " + fileName;
+            MakeEnabledChartsTrue();
+        }
+
+        private void MakeEnabledChartsTrue()
+        {
+            buttonSepalLength.Enabled = true;
+            buttonSepalLength.BackColor = Color.Turquoise;
+            buttonSepalWidth.Enabled = true;
+            buttonSepalWidth.BackColor = Color.Turquoise;
+            buttonPetalLength.Enabled = true;
+            buttonPetalLength.BackColor = Color.Turquoise;
+            buttonPetalWidth.Enabled = true;
+            buttonPetalWidth.BackColor = Color.Turquoise;
+            buttonPieChart.Enabled = true;
+            buttonPieChart.BackColor = Color.Turquoise;
+            richTextBoxErrors.Visible = false;
+        }
+
+        /// <summary>
+        /// Cleans all charts and makes text boxes enabled.
+        /// </summary>
+        private void CleanAll()
+        {
+            sepalLengthChart.Series.Clear();
+            sepalLengthChart.Titles.Clear();
+            sepalWidthChart.Series.Clear();
+            sepalWidthChart.Titles.Clear();
+            petalLengthChart.Series.Clear();
+            petalLengthChart.Titles.Clear();
+            petalWidthChart.Series.Clear();
+            petalWidthChart.Titles.Clear();
+            pieChart.Series.Clear();
+            pieChart.Titles.Clear();
+            buttonSepalLength.Enabled = false;
+            buttonSepalLength.BackColor = Color.Maroon;
+            buttonSepalWidth.Enabled = false;
+            buttonSepalWidth.BackColor = Color.Maroon;
+            buttonPetalLength.Enabled = false;
+            buttonPetalLength.BackColor = Color.Maroon;
+            buttonPetalWidth.Enabled = false;
+            buttonPetalWidth.BackColor = Color.Maroon;
+            buttonPieChart.Enabled = false;
+            buttonPieChart.BackColor = Color.Maroon;
+        }
+
+        /// <summary>
+        /// Makes text box for errors visible, cleanse all charts and text boxes,
+        /// send messege about the problem.
+        /// </summary>
+        /// <param name="ex">Catched exception.</param>
+        private void TellAboutError(Exception ex)
+        {
+            richTextBoxErrors.Visible = true; 
+            CleanAll();
+            richTextBoxErrors.Text = "Error: " + ex.Message;
+        }
+
+        /// <summary>
+        /// Creates a pie chart for distances between irisis' average vectors.
+        /// Shows values of distances, that are rounded up to five decimal places.
+        /// </summary>
+        private void CreatePieChart()
+        {
+            pieChart.Series.Clear();
+            pieChart.Titles.Clear();
+            IrisClass[] iris;
+            double[] values;
+            try { iris = ir.CreateData(fileName); }
+            catch(Exception ex) { TellAboutError(ex); return; }
+            try { values = ex.ExecuteOperation(iris, Executor.Opearations.CREATE_PIE_CHART); }
+            catch(Exception ex) { TellAboutError(ex); return; }
+            var headers = new string[] { "Setosa and VersiColor","Setosa and Virginica",
+                    "Versicolor and Virginica" };
+            pieChart.Titles.Add("Distances");
+            pieChart.Palette = ChartColorPalette.SeaGreen;
+            pieChart.Series.Add(new Series("distances"));
+            pieChart.Series["distances"].ChartType = SeriesChartType.Pie;
+            for (int i = 0; i < values.Length; i++)
+            {
+                pieChart.Series["distances"].Points.AddXY(headers[i], Math.Round(values[i], 5));
+                pieChart.Series["distances"].IsValueShownAsLabel = true;
+            }
+        }
+
+        /// <summary>
+        /// Creates a bar chart for choosed metric.
+        /// </summary>
+        /// <param name="chartNumber">Chart's number.</param>
+        /// <param name="chartHeader">Chart's header.</param>
+        /// <param name="chart">Choosed chart.</param>
+        private void ChooseBarChart(Executor.Opearations chartNumber, string chartHeader, Chart chart)
+        {
+            IrisClass[] iris;
+            double[] values;
+            try { iris = ir.CreateData(fileName); }
+            catch (Exception ex) { TellAboutError(ex); return; ; }
+            try { values = ex.ExecuteOperation(iris, chartNumber); }
+            catch (Exception ex) { TellAboutError(ex); return; }
+            var headers = new string[] { "Setosa", "VersiColor", "Virginica" };
+            chart.Palette = ChartColorPalette.SeaGreen;
+            chart.Titles.Add(chartHeader);
+            for (int i = 0; i < values.Length; i++)
+            {
+                chart.Series.Add(new Series(headers[i]));
+                chart.Series[headers[i]].ChartType = SeriesChartType.Bar;
+                chart.Series[headers[i]].Points.Add(Math.Round(values[i], 5));
+                chart.Series[headers[i]].IsValueShownAsLabel = true;
+            }
+        }
+
+        private void buttonCleanGraphics_Click(object sender, EventArgs e)
+        {
+            CleanAll();
+        }
+    }
+}
